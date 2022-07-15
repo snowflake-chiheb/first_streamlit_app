94cc90f90ce0c25046ca1c28848376f01f62d714
snowflake-chiheb <109364482+snowflake-chiheb@users.noreply.github.com>
2022-07-15 18:17:08 +0200

    Update streamlit_app.py
    we updated the dataframe index to fruit namle

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# set the dataframe index to fruit name 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
