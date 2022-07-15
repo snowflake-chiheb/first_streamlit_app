24b3ce58012ec99ff2d9321fc581022ff7153d59
snowflake-chiheb <109364482+snowflake-chiheb@users.noreply.github.com>
2022-07-15 18:06:10 +0200

    Update streamlit_app.py
    Added the breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Dinner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry otmeal')
+st.text('Kale, Spinach, Rocker smoothie')
+st.text('Hard-boiled Freee Range Egg')
 
