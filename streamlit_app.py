130adf821371ee3fd6fd7226e8bcda9ad4d67cf3
snowflake-chiheb <109364482+snowflake-chiheb@users.noreply.github.com>
2022-07-15 18:12:43 +0200

    Update streamlit_app.py
    Imported pandas 
    added a dataframe to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import pandas as pd 
 
 st.title('My Parents New Healthy Dinner')
 st.header('Breakfast Menu')
@@ -9,3 +10,5 @@ st.text('🐔 Hard-boiled Freee Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
