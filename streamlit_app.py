91bcc2da66d490cad56055e530302dc980e6ae48
snowflake-chiheb <109364482+snowflake-chiheb@users.noreply.github.com>
2022-07-19 17:26:36 +0200

    Update streamlit_app.py
    add an input for the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 # API section 
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ 'kiwi')
+# added an input to chose the fruit
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # load the data into a dataframe 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
